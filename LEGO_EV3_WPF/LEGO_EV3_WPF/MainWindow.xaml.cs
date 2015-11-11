53d6778a90cec9f3c4cc4b268de8e63470538b2b
andrehendriks <weze10bj@kpnmail.nl>
2015-11-11 09:42:24 +0100

    Experimental

---
+++ LEGO_EV3_WPF/LEGO_EV3_WPF/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lego.Ev3.Desktop;
+using Lego.Ev3.Core;
+
+namespace LEGO_EV3_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brick brick = new Brick(new BluetoothCommunication("COM46"));
+
+        public MainWindow()
+        {
+            
+           
+            brick.Ports[InputPort.Four].SetMode(InfraredMode.Proximity);
+
+            InitializeComponent();
+        }
+
+        private async void async()
+        {
+            await brick.ConnectAsync();
+        }
+
+        private async void BatchCommand()
+        {
+            brick.BatchCommand.TurnMotorAtSpeedForTime(OutputPort.B, 50, 1000, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, 50, 1000, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        private async void BatchCommand2()
+        {
+            brick.BatchCommand.TurnMotorAtSpeedForTime(OutputPort.B, -50, 1000, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, 50, 1000, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        private async void BatchCommand3()
+        {
+            brick.BatchCommand.TurnMotorAtSpeedForTime(OutputPort.B, 50, 1000, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, -50, 1000, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        private async void BatchCommand4()
+        {
+            brick.BatchCommand.TurnMotorAtSpeedForTime(OutputPort.B, -50, 1000, false);
+            brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, -50, 1000, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+
+        private async void BatchCommand5()
+        {
+            brick.BatchCommand.StepMotorAtSpeed(OutputPort.B, 0, 100, false);
+            brick.BatchCommand.StepMotorAtSpeed(OutputPort.C, 0, 100, false);
+            await brick.BatchCommand.SendCommandAsync();
+        }
+        
+
+        
+
+        
+
+        
+
+        
+
+        private void button_left_Click(object sender, RoutedEventArgs e)
+        {
+            BatchCommand2();
+        }
+
+        private void button_forward_Click(object sender, RoutedEventArgs e)
+        {
+            BatchCommand();
+        }
+
+        private void button_right_Click(object sender, RoutedEventArgs e)
+        {
+            BatchCommand3();
+        }
+
+        private void button_backward_Click(object sender, RoutedEventArgs e)
+        {
+            BatchCommand4();
+        }
+
+        private void button_stop_Click(object sender, RoutedEventArgs e)
+        {
+            BatchCommand5();
+        }
+
+        private void button_connect_Click(object sender, RoutedEventArgs e)
+        {
+            async();
+        }
+    }
+}
